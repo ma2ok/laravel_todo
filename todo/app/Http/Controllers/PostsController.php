c5b7a412b85f281c5f5a3152286c74aee0236583
Tubasa Matsuoka <workwing0283@gamil.com>
2020-06-23 11:26:34 +0900

    編集てまえ

--- todo/app/Http/Controllers/PostsController.php
+++ todo/app/Http/Controllers/PostsController.php
@@ -11,10 +11,17 @@ class PostsController extends Controller
       // $posts = \App\Post::all();
       // $posts = Post::all();
       // $posts = Post::orderBy('created_at', 'desc')->get();
-    $posts = Post::latest()->get();
+      $posts = Post::latest()->get();
       // $posts = [];
       // dd($posts->toArray()); // dump die
       // return view('posts.index', ['posts' => $posts]);
-    return view('posts.index')->with('posts', $posts);
+      return view('posts.index')->with('posts', $posts);
+    }
+
+    // public function show($id) {
+    public function show(Post $post) {
+      // $post = Post::find($id);
+      // $post = Post::findOrFail($id);
+      return view('posts.show')->with('post', $post);
     }
 }
--- todo/resources/views/posts/index.blade.php
+++ todo/resources/views/posts/index.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>Todo List</title>
+  <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
   <div class="container">
@@ -14,7 +15,10 @@
       @endforeach
       --}}
       @forelse ($posts as $post)
-      <li><a href="">{{ $post->title }}</a></li>
+      <!-- <li><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></li> -->
+      <!-- <li><a href="{{ url('/posts', $post->id) }}">{{ $post->title }}</a></li> -->
+      <!-- <li><a href="{{ action('PostsController@show', $post->id) }}">{{ $post->title }}</a></li> -->
+      <li><a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a></li>
       @empty
       <li>No posts yet</li>
       @endforelse
